a87660eafbda33fbc4ccce0f576e8bb95c75a603
jgcamiloaga <jcamiloc148@gmail.com>
2026-05-20 12:00:00 -0500

    feat: implement core product management and authentication modules with supporting UI components and API services

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "amzn-dtt-s3-rob.s3.us-east-2.amazonaws.com",
+      },
     ],
   },
 };
